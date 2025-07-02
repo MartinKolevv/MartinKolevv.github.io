a1617f7a97d19a10f754a531cd3928b36ef53931
MartinKolevv <martin.kolev12@gmail.com>
2025-07-02 10:41:18 +0300

    Adding API REST

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function main() {
-    console.log("Hello world from Martin Kolev, airing live from Valencia, Spain");
-}
-main();
+async function GetAPIData() {
+            try {
+                const response = await fetch("https://dog.ceo/api/breeds/image/random");
+                if (response.ok) {
+                    const data = await response.json();
+                	document.getElementById("dogimg").src = data["message"];
+                } else {
+                    console.error(`HTTP error ${response.status}`);
+                }
+            } catch (error) {
+                console.error("Fetch failed:", error);
+            }
+        }
+        GetAPIData();
